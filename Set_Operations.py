f80893f85565fcb992567fbef0c011e0da76f815
Murtuza Ali <148961257+Murtuza237@users.noreply.github.com>
2025-06-09 09:12:58 +0530

    Add files via upload

---
+++ Set_Operations.py
@@ -0,0 +1,7 @@
+E={0,1,2,3,4,5,6,8}
+N={2,4,9,10,11,12}
+
+print("\nUnion Of E and N: ",E | N)  # Union
+print("\nIntersection Of E and N: ",E & N)  # Intersection
+print("\nDifference Of E and N: ",E - N)  # Difference
+print("\nSymmetric Difference Of E and N: ",E ^ N)  # Symmetric Difference
